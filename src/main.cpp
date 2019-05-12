426d4a7907bb36126c3f4dd70daa12d645d36399
Andrey Obolensky <45119634+AndrePim@users.noreply.github.com>
2019-05-12 18:30:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 
-int sensorPin = A0;  // Пин дла датчика освещённости
+int sensor_Light_Pin = A0;  // Пин дла датчика освещённости
 int ledPin = 13;     // Пин для светодиода
-int sensorValue = 0; // Переменная для сохранения значения, поступающего с датчика
-int analog = 500;    // Пороговое значение для срабатывание датчика
+int sensor_Light_Value = 0; // Переменная для сохранения значения, поступающего с датчика
+int sensor_Light_Analog = 500;    // Пороговое значение для срабатывание датчика
 
 void setup() {
   Serial.begin(9600);      // Инициализация последовательного соединения и задание скорости передачи данных в бит/с(бод)
-  pinMode(ledPin, OUTPUT); // Установка режима работы пина светодиода на выход}
+  pinMode(ledPin, OUTPUT); // Установка режима работы пина светодиода на выход
 }
 
 void loop() {
-  sensorValue = analogRead(sensorPin); // Чтение значения с датчика
+  sensor_Light_Value = analogRead(sensor_Light_Pin); // Чтение значения с датчика
   // Сравнение значения с порта А0 с пороговым значением
-  if (sensorValue < analog) {
+  if (sensor_Light_Value < sensor_Light_Analog) {
     Serial.println("Light on!"); // Вывод надписи в мониторе последовательного порта
     digitalWrite(ledPin, HIGH);  // Загорается светодиод
   }
